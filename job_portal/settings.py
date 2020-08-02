e54bc049ab119bd1ec77ab0842c2710a016f8489
prithvirajbytes <biswasprithviraj74@gmail.com>
2020-08-02 15:04:18 +0530

    first commit

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -30,15 +30,13 @@ ALLOWED_HOSTS = []
 # Email Settings
 
 
-SEND_GRID_API_KEY = ''
+
+# Twilio SendGrid
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = '' #sendgrid email
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'nazmul199512@gmail.com' #sendgrig email
-ACCOUNT_EMAIL_SUBJECT_PREFIX = 'Contact email from CAREERS'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('SKd2f3e79ce36cf4b32ab5ad8f3c90ec58')
 
 
 # Application definition
